961313e69114d76e950755e79e846e56e1101452
Sara-Sommer <Tallyho944@gmail.com>
2017-10-26 17:28:14 -0700

    Coding

--- src/main/java/us/ftcteam11574/teamcode2017/Mecanum_Drive_2017.java
+++ src/main/java/us/ftcteam11574/teamcode2017/Mecanum_Drive_2017.java
@@ -8,9 +8,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Mecanum_Drive_2017", group = "Mecanum")
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class Mecanum_Drive_2017 extends OpMode {
-    DcMotor mFL, mBL, mFR, mBR;
-    Servo SR;
-    Servo SL;
+    public static final double CLAW_CLOSED_POSITION = 1.0;
+    public static final double CLAW_OPEN_POSITION = 0.0;
+    DcMotor mFL, mBL, mFR, mBR, mLS;
+    Servo SR, SL;
 
     @Override
     public void init() {
@@ -18,16 +19,19 @@ public class Mecanum_Drive_2017 extends OpMode {
         mBL = hardwareMap.dcMotor.get("mBL");
         mFR = hardwareMap.dcMotor.get("mFR");
         mBR = hardwareMap.dcMotor.get("mBR");
+        mLS = hardwareMap.dcMotor.get("mLS");
 
         mFL.setDirection(DcMotor.Direction.FORWARD);
         mBL.setDirection(DcMotor.Direction.FORWARD);
         mFR.setDirection(DcMotor.Direction.REVERSE);
         mBR.setDirection(DcMotor.Direction.REVERSE);
+        mLS.setDirection(DcMotor.Direction.FORWARD);
 
         mFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         mBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         mFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         mBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mLS.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         SR = hardwareMap.servo.get("SR");
         SL = hardwareMap.servo.get("SL");
@@ -40,61 +44,30 @@ public class Mecanum_Drive_2017 extends OpMode {
     public void loop() {
         double motorPower = 1.0;
         double slowMotorPower = 0.30;
-        double xs = -gamepad1.left_stick_x;
-        double ys = -gamepad1.left_stick_y;
-        double lt = -gamepad1.left_trigger;
-        double rt = -gamepad1.right_trigger;
-        boolean dpd = gamepad1.dpad_down;
-        boolean dpu = gamepad1.dpad_up;
-        boolean dpl = gamepad1.dpad_left;
-        boolean dpr = gamepad1.dpad_right;
+        double DriveX = -gamepad1.right_stick_x;
+        double DriveY = -gamepad1.right_stick_y;
+        double SpinLeft = -gamepad1.left_trigger;
+        double SpinRight = -gamepad1.right_trigger;
+        double LiftSlide = gamepad1.left_stick_y;
+        boolean OpenClaw = gamepad1.x;
+        boolean CloseClaw = gamepad1.y;
 
-        if (gamepad1.dpad_left) {
-            // Strafe left.
-            mFL.setPower(-motorPower);
-            mBL.setPower(motorPower);
-            mFR.setPower(motorPower);
-            mBR.setPower(-motorPower);
-        } else if (gamepad1.dpad_right) {
-            // Strafe right.
-            mFL.setPower(motorPower);
-            mBL.setPower(-motorPower);
-            mFR.setPower(-motorPower);
-            mBR.setPower(motorPower);
-        } else if (gamepad1.dpad_up) {
-            // Drive forward.
-            mFL.setPower(motorPower);
-            mBL.setPower(motorPower);
-            mFR.setPower(motorPower);
-            mBR.setPower(motorPower);
-        } else if (gamepad1.dpad_down) {
-            // Drive backward.
-            mFL.setPower(-motorPower);
-            mBL.setPower(-motorPower);
-            mFR.setPower(-motorPower);
-            mBR.setPower(-motorPower);
-        } else if(gamepad1.left_trigger > 0.5) {
-            //Spin Left
-            mFL.setPower(-motorPower);
-            mBL.setPower(-motorPower);
-            mFR.setPower(motorPower);
-            mBR.setPower(motorPower);
-        } else if(gamepad1.right_trigger > 0.5) {
-            //Spin Right
-            mFL.setPower(motorPower);
-            mBL.setPower(motorPower);
-            mFR.setPower(-motorPower);
-            mBR.setPower(-motorPower);
-        } else {
+        if (OpenClaw) {
+            SL.setPosition(CLAW_OPEN_POSITION);
+            SR.setPosition(CLAW_OPEN_POSITION);
+        }else if (CloseClaw){
+            SL.setPosition(CLAW_CLOSED_POSITION);
+            SR.setPosition(CLAW_CLOSED_POSITION);
+        }
             // Allow use of the analog left/right sticks to control the robot in differential
             // steering (tank driving) mode. Allow use of the left and right trigger buttons
             // to rotate the robot.
-            mFL.setPower((ys - xs + lt - rt) * slowMotorPower);
-            mBL.setPower((ys + xs + lt - rt) * slowMotorPower);
-            mFR.setPower((ys + xs - lt + rt) * slowMotorPower);
-            mBR.setPower((ys - xs - lt + rt) * slowMotorPower);
-
-        }
+            mFL.setPower(DriveY - DriveX + SpinLeft - SpinRight);
+            mBL.setPower(DriveY + DriveX + SpinLeft - SpinRight);
+            mFR.setPower(DriveY + DriveX - SpinLeft + SpinRight);
+            mBR.setPower(DriveY - DriveX - SpinLeft + SpinRight);
+            mLS.setPower(LiftSlide);
+            mLS.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         float l = (gamepad1.left_stick_x);
         float r = (gamepad1.right_stick_x * -1);
