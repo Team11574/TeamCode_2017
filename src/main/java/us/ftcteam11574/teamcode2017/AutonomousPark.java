9a1e22ef980face67e59e5d7fd15932e52bf52f7
Sara-Sommer <Tallyho944@gmail.com>
2017-11-03 12:00:46 -0700

    Auto for parking

---
+++ src/main/java/us/ftcteam11574/teamcode2017/AutonomousPark.java
@@ -0,0 +1,14 @@
+package us.ftcteam11574.teamcode2017;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="AutonomousPark", group="Autonomous")
+@SuppressWarnings("unused")
+public class AutonomousPark extends Generic_Drive {
+    @Override
+    public void robotRun() {
+        // Drive to park in front of Cryptobox.
+        drive_distance(DRIVE_FORWARD, 34.0, 0.8);
+        stop_all_motors();
+    }
+}
