92dcce5c8bb2537ac2ccb6b7144de8bb2fe18b97
Sara-Sommer <Tallyho944@gmail.com>
2017-12-04 13:05:42 -0800

    Created new autonomous

---
+++ src/main/java/us/ftcteam11574/teamcode2017/AutonomousGlyphAndPark.java
@@ -0,0 +1,13 @@
+package us.ftcteam11574.teamcode2017;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by Sara N on 12/1/2017.
+ */
+
+@Autonomous(name = "Autonomous Glyph And Park")
+public class AutonomousGlyphAndPark extends Generic_Drive {
+
+
+}
